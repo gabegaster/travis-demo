02967ba735c7a71b361e9030a5ccd40cffd1a6ab
gabe gaster <gabe@gabegaster.com>
2015-07-24 13:15:44 -0500

    import main, print stuff

--- gabe_travis_demo/main.py
+++ gabe_travis_demo/main.py
@@ -2,8 +2,8 @@ from collections import Counter
 import random
 
 def main():
-    stuff = Counter(random.randint(1,100) for _ in xrange(1000))
-    return sorted(stuff.items())
+    stuff = Counter(random.randint(1,2) for _ in xrange(1000))
+    for i in sorted(stuff.items()): print i
 
 if __name__=="__main__":
     main()
