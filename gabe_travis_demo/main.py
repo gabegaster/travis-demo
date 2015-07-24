b1728ff295e73f61575123eed8f19e5fac1abe8d
gabe gaster <gabe@gabegaster.com>
2015-07-24 13:34:24 -0500

    ran pep8

--- gabe_travis_demo/main.py
+++ gabe_travis_demo/main.py
@@ -1,10 +1,12 @@
 from collections import Counter
 import random
 
+
 def main():
-    stuff = Counter(random.randint(1,2) for _ in xrange(1000))
-    for i in sorted(stuff.items()): yield i
+    stuff = Counter(random.randint(1, 2) for _ in xrange(1000))
+    for i in sorted(stuff.items()):
+        yield i
 
-if __name__=="__main__":
-    for j in main(): print j
-
+if __name__ == "__main__":
+    for j in main():
+        print j
